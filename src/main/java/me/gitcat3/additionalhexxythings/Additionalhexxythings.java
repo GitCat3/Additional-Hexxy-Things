175db8b652e367c14b01bca69183f5478e5924a5
GitCat3 <hulkbuster119@gmail.com>
2025-06-17 19:46:38 -0400

    initial commit, contains everything needed for the mod to boot with hex casting

---
+++ src/main/java/me/gitcat3/additionalhexxythings/Additionalhexxythings.java
@@ -0,0 +1,10 @@
+package me.gitcat3.additionalhexxythings;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Additionalhexxythings implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/me/gitcat3/additionalhexxythings/client/AdditionalhexxythingsClient.java
@@ -0,0 +1,10 @@
+package me.gitcat3.additionalhexxythings.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AdditionalhexxythingsClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
